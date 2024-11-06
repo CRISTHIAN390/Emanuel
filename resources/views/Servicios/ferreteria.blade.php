77ecd037abfc74a6647b38058e584d3421f783a1
Cristhian <70170324+CRISTHIAN390@users.noreply.github.com>
2024-11-06 17:49:57 -0500

    EstructuraComplejoDeportivo

--- resources/views/Servicios/ferreteria.blade.php
+++ resources/views/Servicios/ferreteria.blade.php
@@ -10,18 +10,18 @@
                 <h2 class="text-center text-3xl font-bold text-gray-800 mb-6" style="font-family: Cooper Black">COMPLEJO
                     DEPORTIVO</h2>
                 <p class="text-center text-gray-600 mb-8">
-                    Nuestro complejo deportivo cuenta con una amplia selección de equipos y accesorios,
-                    lo que lo hace una excelente opción para todos los deportistas, no obstante cuenta con una excelente
-                    seguridad y confidencialidad.
+                    En nuestro complejo deportivo, ofrecemos una experiencia única
+                     y accesible para deportistas de todas las edades.
+                      Con una amplia variedad de equipos modernos y espacios seguros
+                      , garantizamos un ambiente ideal para que los jóvenes
+                       y deportistas disfruten al máximo de sus actividades favoritas.
                 </p>
                 <!-- Contenedor adaptable de imagen y texto -->
                 <div class="grid gap-6 lg:grid-cols-2 lg:space-x-6 animate__animated animate__fadeInUp">
                 <!-- Texto centrado verticalmente -->
                 <div class="order-2 lg:order-1 flex items-center justify-center">
                     <p class="text-center p-4 bg-white rounded-xl shadow-lg transform transition duration-500 hover:scale-105 hover:shadow-2xl">
-                        Nuestro complejo deportivo cuenta con una amplia selección de equipos y accesorios,
-                        lo que lo hace una excelente opción para todos los deportistas, no obstante cuenta con una
-                        excelente seguridad y confidencialidad.
+                         ********************
                         
                     </p>
                 </div>
