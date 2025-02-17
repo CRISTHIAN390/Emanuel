4016e0fd1050e465cb9c337bf0965ea934675830
Cristhian <xcrissx12345@gmail.com>
2025-02-17 09:11:24 -0500

    Correoxl2

--- resources/views/emails/consulta.blade.php
+++ resources/views/emails/consulta.blade.php
@@ -1,4 +1,5 @@
-<h1 style="color: #333; font-size: 24px;">Nueva Consulta Recibida</h1>
+<h1 style="color: #000000; font-size: 24px;">Grupo Emanuel RLG</h1>
+<h4 style="color: #ff7b00; font-size: 15px;">Consulta Recibida Por</h4>
 <p><strong>Nombre:</strong> {{ $details['name'] }}</p>
 <p><strong>Email:</strong> {{ $details['email'] }}</p>
 <p><strong>Mensaje:</strong></p>
