21a8b10ee7b7061dc8f4ddd8ab7e0928c0da37af
Cristhian <xcrissx12345@gmail.com>
2025-04-14 16:54:11 -0500

    ConfigLogximg

--- resources/views/Servicios/complejo.blade.php
+++ resources/views/Servicios/complejo.blade.php
@@ -58,7 +58,7 @@
                     <div class="grid gap-6 lg:grid-cols-2 lg:space-x-6 animate__animated animate__fadeInUp mt-6">
                         <!-- Imagen -->
                         <div class="order-1 lg:order-1 flex justify-center">
-                            <img src="/img/Publicidad/imagencomplejo4.jpeg" alt="Complejo deportivo"
+                            <img src="/img/Publicidad/Comple1.jpg" alt="Complejo deportivo"
                                 class="w-full rounded-xl shadow-lg transition-transform duration-500 transform group-hover:scale-105 hover:shadow-2xl   hover:shadow-[#FFD700]">
                         </div>
                         <!-- Texto en tarjeta solo en pantallas grandes -->
